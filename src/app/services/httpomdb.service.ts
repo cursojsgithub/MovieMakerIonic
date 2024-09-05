37eca9920d695ab5334993dbc0aa96ff0f9b3d72
cursojsgithub <cursojsgithub@gmail.com>
2024-09-05 16:05:36 +0200

    tercer commit

--- src/app/services/httpomdb.service.ts
+++ src/app/services/httpomdb.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Inject, inject, Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+
+const BASE_URL = "https://www.omdbapi.com";
+const API_KEY = environment.OMDB_API_KEY;
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class HttpomdbService {
+
 
   constructor() { }
 }
